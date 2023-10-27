40de166a583bd538bdfef7860f671cd6a1117c0a
yacine <yacine_said13@hotmail.com>
2023-10-27 14:41:51 +0200

    classe world et render prêtes

--- src/Render.py
+++ src/Render.py
@@ -1,11 +1,134 @@
 import pygame
+from World import World
+
+
+# Define the screen dimensions
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Your Game Title')
 
 class Render:
-    def __init__(self) -> None:
+    def __init__(self, n, m) -> None:
+        self.world = World(n,m)
+        self.n = n
+        self.m = m
         self.list_x_y = [[150 + x * 10 - y * 10, 100 + x * 5 + y * 5] for x in range(self.n) for y in range(self.m)]
         self.image = pygame.image.load('data/images/grass.png').convert()
         self.image.set_colorkey((0, 0, 0))
         for i in self.list_x_y:
             self.rect = self.image.get_rect()
             self.rect.center = i
+            
+            
+        self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2()
+        self.half_w = self.display_surface.get_size()[0] // 2
+        self.half_h = self.display_surface.get_size()[1] // 2
+
+        self.camera_borders = {'left': 200,'right': 200, 'top': 100, 'bottom': 100}
+        l = self.camera_borders['left']
+        t = self.camera_borders['top']
+        w = self.display_surface.get_size()[0] - (self.camera_borders['left'] + self.camera_borders['right'])
+        h = self.display_surface.get_size()[1] - (self.camera_borders['top'] + self.camera_borders['bottom'])
+        self.camera_rect = pygame.Rect(l, t, w, h)
+
         
+        self.keyboard_speed = 15
+        self.mouse_speed = 0.2
+
+        self.zoom_scale = 1
+        self.internal_surf_size = (2500, 2500)
+        self.internal_surf = pygame.Surface(self.internal_surf_size, pygame.SRCALPHA)
+        self.internal_rect = self.internal_surf.get_rect(center=(self.half_w, self.half_h))
+        self.internal_surface_size_vector = pygame.math.Vector2(self.internal_surf_size)
+        self.internal_offset = pygame.math.Vector2()
+        self.internal_offset.x = self.internal_surf_size[0] // 2 - self.half_w
+        self.internal_offset.y = self.internal_surf_size[1] // 2 - self.half_h
+    
+    def draw(self):
+        self.keyboard_control()
+        self.zoom_keyboard_control()
+        
+        screen.fill((255, 255, 255))
+        for i in self.list_x_y:
+            #screen.blit(self.image, i)
+            vect_list = [x * self.zoom_scale for x in (self.rect.width ,self.rect.height)]
+            image=pygame.transform.scale(self.image, vect_list)
+            screen.blit(image, [(i[0]-self.offset.x)*self.zoom_scale,(i[1]-self.offset.y)* self.zoom_scale])
+
+        for k, v in self.world.world.items():
+            for obj in v:
+                #screen.blit(obj.image, list(eval(k)))           
+                vect_list_obj = [x * self.zoom_scale for x in (pygame.Surface.get_width(obj.image),pygame.Surface.get_height(obj.image))]
+                image_obj=pygame.transform.scale(obj.image, vect_list_obj)
+                screen.blit(image_obj, [(list(eval(k))[0]-self.offset.x)*self.zoom_scale,(list(eval(k))[1]-self.offset.y)*self.zoom_scale])
+                obj.current_sprite = 0 if  obj.current_sprite == 3 else obj.current_sprite+1
+                obj.image = obj.sprites[obj.current_sprite]
+                
+        pygame.display.flip()
+    
+   
+    
+    
+    def keyboard_control(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.camera_rect.x -= self.keyboard_speed
+        if keys[pygame.K_RIGHT]:
+            self.camera_rect.x += self.keyboard_speed
+        if keys[pygame.K_UP]:
+            self.camera_rect.y -= self.keyboard_speed
+        if keys[pygame.K_DOWN]:
+            self.camera_rect.y += self.keyboard_speed
+
+        self.offset.x = self.camera_rect.left - self.camera_borders['left']
+        self.offset.y = self.camera_rect.top - self.camera_borders['top']
+
+    def zoom_keyboard_control(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_c]:
+            self.zoom_scale += 0.1
+        if keys[pygame.K_x]:
+            self.zoom_scale -= 0.1
+        
+        
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self,id):
+        super().__init__()
+        self.id=id
+        self.sprites = []
+        self.sprites.append(pygame.image.load('data/images/0.png').convert())
+        self.sprites.append(pygame.image.load('data/images/1.png').convert())
+        self.sprites.append(pygame.image.load('data/images/2.png').convert())
+        self.sprites.append(pygame.image.load('data/images/3.png').convert())
+        self.current_sprite = 0
+        self.image = self.sprites[0]
+        self.rect = self.image.get_rect()
+        self.rect.center = list([0, 0])
+
+r = Render(30,30)
+object1 = GameObject(1)
+object2 = GameObject(2)
+object3 = GameObject(3)
+
+listObjects = [object1, object2, object3]
+
+for i in listObjects:
+    r.world.addOnMap(i)
+
+running = True
+clock = pygame.time.Clock()
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    r.draw()
+    
+    for i in listObjects:
+        r.world.move(i)
+    
+    clock.tick(20)
+    
+pygame.quit()
--- src/World.py
+++ src/World.py
@@ -5,34 +5,31 @@ import random
 # Initialize Pygame
 pygame.init()
 
-# Define the screen dimensions
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Your Game Title')
+
 
 class World:
     def __init__(self, n, m):
+        self.world = defaultdict(list)
         self.n = n
         self.m = m
-        self.world = defaultdict(list)
+        self.list_x_y = [[150 + x * 10 - y * 10, 100 + x * 5 + y * 5] for x in range(self.n) for y in range(self.m)]
+        
         
 
     def addOnMap(self, _Object):
         
-        listKeys=#[i for i in self.world.keys()]
+        listKeys=[i for i in self.world.keys()]
         position = random.choice(self.list_x_y)
         
         if str(position) in listKeys:
             print(f"{position} is in {self.world.keys()} so there is a collision")
             
         self.world[str(position)].append(_Object)
-        #print("self.world.keys() = ",[i for i in self.world.keys()])
         
         _Object.rect.center = list(position)
         
-        #print("type (obj.rect.center) = ", type(_Object.rect.center))
-        #print("addonmap => ", self.world)
 
+    
     def availableNearbyPosition(self, Position):
         l = [[(Position[0] - dup1), (Position[1] - dup2)]
              for dup1 in (-10, 10) for dup2 in (-5, 5)]
@@ -43,23 +40,20 @@ class World:
                     res.append(i)
         return random.choice(res)
 
-    def move(self, _Object): 
+    def move(self, _Object):
+        
         listValue = [i[j] for i in self.world.values() for j in range(len(i))]+[]
         if _Object.id not in [i.id for i in listValue]:
             self.addOnMap(_Object)
-            print(_Object, "is not in", listValue)
-            print("_Object.rect.center =", _Object.rect.center, "world = ",self.world)
+        
         tempDict = defaultdict(list)
         tempDict = self.world.copy()
         Position = self.availableNearbyPosition(list(_Object.rect.center))
-        #print("newPosition = ", Position)
-        #print("before changes :  self.world = ", self.world)
+
         
         #retirer object de son ancienne positiion (=retirer de la key)
         for key, value in self.world.items():
             if key == str(list(_Object.rect.center)) and _Object in [i for i in value]:
-                #print("TROUVÉÉÉÉÉÉÉÉÉÉÉÉ   object = ",_Object," and key = ",key)
-                #on change cette valeur sur le tompons tempDict
                 tempDict[str(list(_Object.rect.center))]=[]
                 for i in value :
                     if i != _Object:
@@ -72,69 +66,8 @@ class World:
         tempDict[str(Position)].append(_Object)
         _Object.rect.center = list(Position)
         
-        #on renouvel maintenant la valeur de self.world
         self.world = tempDict.copy()
-        #print("after changes :  self.world = ", self.world)
-        
-        #print("HEYYY world = ", self.world)
 
-    def draw(self):
-        screen.fill((255, 255, 255))
-        for i in self.list_x_y:
-            screen.blit(self.image, i)
-            #grass_image.set_colorkey((0, 0, 0))
-
-        for k, v in self.world.items():
-            for obj in v:
-                screen.blit(obj.image, list(eval(k)))
-                #print("\neval(k) = ", eval(k))
-                #print("obj.rect.center = ", obj.rect.center)                
-                obj.current_sprite+=1
-
-        pygame.display.flip()
-
-class GameObject(pygame.sprite.Sprite):
-    def __init__(self,id):
-        super().__init__()
-        self.id=id
-        self.sprites = []
-        self.sprites.append(pygame.image.load('data/images/0.png').convert())
-        self.sprites.append(pygame.image.load('data/images/1.png').convert())
-        self.sprites.append(pygame.image.load('data/images/2.png').convert())
-        self.sprites.append(pygame.image.load('data/images/3.png').convert())
-        self.current_sprite = 0
-        
-        if self.current_sprite > 3:
-            self.current_sprite=0
-        self.image = self.sprites[self.current_sprite]
-        self.rect = self.image.get_rect()
-        self.rect.center = list([0, 0])
-
-world = World(n=30, m=30)
-object1 = GameObject(1)
-object2 = GameObject(2)
-object3 = GameObject(3)
-
-world.addOnMap(object1)
-world.addOnMap(object2)
-world.addOnMap(object3)
-
-running = True
-clock = pygame.time.Clock()
-
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    world.draw()
-    
-    world.move(object1)
-    world.move(object2)
-    world.move(object3)
-    
-    clock.tick(30)
-    
 pygame.quit()
 
 
