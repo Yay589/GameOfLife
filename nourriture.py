a46e39f753e516fd023bd876de572fdce984d0de
Celestine Raveneau <celestine.raveneau@gmail.com>
2024-01-10 09:12:53 +0100

    version avec perception & mémoire é reproduction sexuée

--- nourriture.py
+++ nourriture.py
@@ -2,17 +2,36 @@ from case import Case
 from random import *
 from parametre import *
 
-class Nourriture():
-    #objet nourriture prend en paramètre optionnel la quantié d'énergie (par défaut = qtité définie dans parametre)
-    def __init__(self,foodEnergy = foodE, coord = (randint(0,N-1),randint(0,N-1))):
-        self.energy = foodEnergy
-        self.coordonnee = coord
-        if(coord not in grille):
-            self.case = Case(coord,qtite_nourriture = self.energy)
-            grille[coord] = self.case
-        else:
-            self.case = grille[coord]
-            self.case.ajouterNourriture(self.energy)
+def ajouterNourritureGrille(nombreNourriture = numberFood, energieNourriture = foodE):
+    for i in range (nombreNourriture) :
+        ajouterNourritureCase(energieNourriture)
 
-    def speak(self): #juste pour faire des tests
-        print("(Food) Je suis en : ",self.coordonnee, "J'ai",self.energy,"energie")
+def ajouterNourritureCase(energieNourriture = foodE):
+    coord = (randint(0,N-1),randint(0,N-1))
+    
+    if(coord not in grille):
+        case = Case(coord)
+        case.ajouterNourriture(energieNourriture)
+    else :
+        case = grille[coord]
+        case.ajouterNourriture(energieNourriture)
+
+def ajouterNourritureCaseSpecifique(coordCase, qtite_nourriture = foodE):
+        if(coordCase not in grille):
+            Case(coord = coordCase, qtite_nourriture = qtite_nourriture)
+        else :
+            grille[coordCase].qtite_nourriture += qtite_nourriture
+#la fonction ajouterNourriture pour une case est dans case.py
+
+def viderNourritureGrille():
+    caseSuprCoord = []
+    for c in grille:
+        grille[c].viderNourriture()
+        if (grille[c].estVide()):
+            caseSuprCoord.append(c)
+    for cSupr in caseSuprCoord: #on est obligés de supprimer après vu qu'on peut pas modifier un dictionnaire en même temps qu'on le parcourt
+        grille[cSupr].supprimer()
+
+def renouvellerNourriture(nombreNourriture = numberFood, energieNourriture = foodE) :
+    viderNourritureGrille()
+    ajouterNourritureGrille(nombreNourriture, energieNourriture)
