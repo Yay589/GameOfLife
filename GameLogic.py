ae9ba52923e87a41f0554d95b4044aff01bd91d8
Slnrtk <cedricrakotomalala@gmail.com>
2024-01-27 12:01:41 +0100

    GameLogic.py

--- GameLogic.py
+++ GameLogic.py
@@ -1,25 +1,58 @@
-from random import *
+import pickle
 from parametre import *
 from math import trunc
-from nourriture import Nourriture
+from nourriture import *
 from case import Case
 from bob import Bob
+from affichage import *
+import collections
+import time
+import random
+
+
+# fonction pour avoir des infos globales sur notre grille
+def avgSpeed():
+    i = 0
+    speedSum = 0
+    for c in grille:
+        for b in grille[c].bobs:
+            speedSum += b.speed
+            i += 1
+    if (i == 0):
+        print("Tous les bobs sont morts")
+        return (-1)
+    else:
+        return speedSum / i
 
 
 if __name__ == '__main__':
-    #game = Game()
-    #game.run()
+    # for x in range(T):
+    for i in range(100):
+        allBobs.append(Bob(coord=(randint(0, N - 1), randint(0, N - 1)), bobPerception=10))
+        # allBobs[i].speak()
 
-    bob1 = Bob()
-    bob2 = Bob(BobEnergy=150,coord=[0,100])
-    bob3 = Bob()
+    j = 0
+    for j in range(7):
+        # os.system("clear")
+        print("Debut de journée")
+        if (len(allBobs) == 0):
+            break
+        j+=1
+        renouvellerNourriture()
+        for k in range(T):
+            print("\033[H\033[J", end="")
+            afficheGrilleSimpleCouleur()
+            time.sleep(0.5)
+            random.shuffle(allBobs)
+            for b in allBobs:
+                if not b.dejaJoue() and not b.seProteger() and not b.attack() and not b.manger() and not b.reproduction():
+                    b.bobDeplacement()
+                    b.speak()
 
-    for i in range(10):
-        bob2.speak()
-        bob2.bouger()
 
-    food1 = Nourriture()
-    food1.speak()
-    bob2.manger(food1)
-    bob2.speak()
-    food1.speak()
+        afficheGrilleSimpleCouleur()
+
+
+# print("fin des 10 jours : ")
+#print("vitesse moyenne des bobs : ",avgSpeed())
+print("nombre de bob en vie : ", len(allBobs))
